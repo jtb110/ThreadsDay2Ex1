33d8b0f64176743e82fe3381e55d6315c1301f11
jtb110 <jtb110@hotmail.com>
2015-08-27 17:49:27 +0200

    Initial commit

---
+++ src/threadsday2ex1/Threads.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadsday2ex1;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Threads implements Runnable {
+
+    private byte[] size;
+    private long sum = 0;
+    String ThisURL;
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    public Threads(String url) {
+        this.ThisURL = url;
+    }
+
+    public void run() {
+        try {
+            InputStream input = new URL(getThisURL()).openStream();
+            byte[] bytebuff = new byte[4096];
+            int read;
+            while ((read = input.read(bytebuff)) > 0) {
+                out.write(bytebuff, 0, read);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            size = out.toByteArray();
+
+            for (byte c : size) {
+                sum += c;
+
+            }
+        }
+    }
+
+    public String getThisURL() {
+        return ThisURL;
+    }
+
+    public void setThisURL(String ThisURL) {
+        this.ThisURL = ThisURL;
+    }
+
+    public void resetSUM() {
+        sum = 0;
+    }
+
+}
---
+++ src/threadsday2ex1/ThreadsDay2Ex1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package threadsday2ex1;
+
+/**
+ *
+ * @author Jonas
+ */
+public class ThreadsDay2Ex1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Threads t1 = new Threads("https://fronter.com/cphbusiness/design_images/images.php/Classic/login/fronter_big-logo.png");
+        Threads t2 = new Threads("https://fronter.com/cphbusiness/design_images/images.php/Classic/login/folder-image-transp.png");
+        Threads t3 = new Threads("https://fronter.com/volY12-cache/cache/img/design_images/Classic/other_images/button_bg.png");
+
+        
+    }
+
+}
